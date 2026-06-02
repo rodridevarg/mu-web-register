679a6bdb15c46e2c4d7f86febe3dd43aa6c50c3f
Rodrigo Rodriguez <rodrigo.desarrollador@gmail.com>
2026-06-01 21:04:12 -0300

    feat(web): add live market prices widget to home, fix download page, update client zip path

--- server.js
+++ server.js
@@ -261,7 +261,7 @@ app.get('/download', (req, res) => {
 
 // Serve client zip file
 app.get('/download/client', (req, res) => {
-  const filePath = path.join(__dirname, 'public', 'downloads', 'LiberMU_Client_v2.zip');
+  const filePath = path.join(__dirname, 'public', 'downloads', 'LiberMU_Client.zip');
   if (require('fs').existsSync(filePath)) {
     res.download(filePath, 'LiberMU_Client.zip');
   } else {
